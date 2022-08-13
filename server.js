0fa8624016e7accb5c4d43e514c43e2f3292ab7c
rpruazol <rpruazol@gmail.com>
2022-08-13 18:15:22 -0400

    modularized weather route

--- server.js
+++ server.js
@@ -4,8 +4,7 @@
 const axios = require('axios');
 const cors = require('cors');
 const express = require('express');
-const weatherData = require('./data/weather.json');
-
+const {getWeather} = require('./lib/weather.js')
 
 require('dotenv').config();
 
@@ -18,7 +17,7 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.get('/weather', (req, res, next) => getWeather(req.query, res, next))
+app.get('/weather', getWeather);
 app.get('/movies', (req, res, next) => getMovies(req.query, res, next))
 
 
@@ -26,18 +25,6 @@ app.listen(PORT, () => {
   console.log('now listening on port ', PORT)
 })
 
-
-async function getWeather(req, res, next) {
-
-  try {
-    const URL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${req.lat}&lon=${req.lon}`;
-    const result = await axios.get(URL)
-    const forecastArray = result.data.data.map(obj => new Forecast(obj));
-    res.status(200).send(forecastArray)
-  } catch (error) {
-    next(error.message)
-  }
-}
 
 async function getMovies(req, res, next){
   const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${req.searchQuery}&page=1`
@@ -51,12 +38,7 @@ async function getMovies(req, res, next){
 }
 
 
-class Forecast {
-  constructor(obj) {
-    this.description = `Low of ${obj.min_temp}, High of ${obj.high_temp} with ${obj.weather.description}`;
-    this.date = obj.valid_date;
-  }
-}
+
 
 
 class Movie {
